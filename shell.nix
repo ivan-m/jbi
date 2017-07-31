81f6cad2b36a5ddf71fca7c9f6649afde85ea2b9
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-07-31 14:42:13 +1000

    Initial project metadata

---
+++ shell.nix
@@ -0,0 +1,25 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, base, stdenv, turtle }:
+      mkDerivation {
+        pname = "jbi";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [ aeson base turtle ];
+        description = "Just Build It - a \"do what I mean\" abstraction for Haskell build tools";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
