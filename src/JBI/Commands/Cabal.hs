1153bfb83707057623ba2eb4288a6f3026f9f7d7
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-08-04 16:35:52 +1000

    Equivalent to the elisp f-traverse-upwards
    Keep recursing up until you find a file that matches the predicate.

--- src/JBI/Commands/Cabal.hs
+++ src/JBI/Commands/Cabal.hs
@@ -16,6 +16,12 @@ import JBI.Commands.Tool
 import JBI.Environment.Global
 import JBI.Tagged
 
+import Control.Exception (SomeException(SomeException), handle)
+import Control.Monad     (filterM)
+import System.Directory  (doesFileExist, getCurrentDirectory, listDirectory)
+import System.FilePath   (dropTrailingPathSeparator, isDrive, takeDirectory,
+                          (</>))
+
 --------------------------------------------------------------------------------
 
 data Cabal mode
@@ -26,3 +32,24 @@ instance Tool (Cabal mode) where
 class CabalMode mode where
   cabalProjectRoot :: Tagged (Cabal mode) CommandPath
                       -> IO (Maybe (Tagged (Cabal mode) ProjectRoot))
+
+--------------------------------------------------------------------------------
+
+-- | If an exception occurs, return 'Nothing'
+tryIO :: IO (Maybe a) -> IO (Maybe a)
+tryIO = handle (\SomeException{} -> return Nothing)
+
+-- | Recurse up until you find a directory containing a file that
+--   matches the predicate, returning that directory.
+recurseUpFindFile :: (FilePath -> Bool) -> IO (Maybe FilePath)
+recurseUpFindFile p = tryIO $ go . dropTrailingPathSeparator =<< getCurrentDirectory
+  where
+    go dir = do cntns  <- listDirectory dir
+                files <- filterM (doesFileExist . (dir </>)) cntns
+                if any p files
+                   then return (Just dir)
+                   -- We do the base case check here so we can
+                   -- actually check the top level directory.
+                   else if isDrive dir
+                           then return Nothing
+                           else go (takeDirectory dir)
