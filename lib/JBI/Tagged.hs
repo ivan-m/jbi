4edf8bec834af3f8500acddb7050111aa99c0147
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-09-04 15:18:10 +1000

    GHC 7.10 seems pickier about Coercible
    Hopefully these extra hints will suffice.

--- lib/JBI/Tagged.hs
+++ lib/JBI/Tagged.hs
@@ -31,13 +31,15 @@ class WithTagged (g :: * -> *) where
   -- | Strip off type safety, run the function, put type safety back on.
   withTaggedF :: (Coercible a a', Coercible b b', Functor f)
                  => (a' -> f (g b')) -> Tagged t a -> f (g (Tagged t b))
-  default withTaggedF :: (Coercible a a', Coercible b b', Functor f
-                         , Coercible (g b') (g (Tagged t b)))
+  default withTaggedF :: ( Coercible a a', Coercible b b', Functor f
+                         , Coercible (g b') (g (Tagged t b))
+                         , Coercible (g (Tagged t b)) (g b'))
                          => (a' -> f (g b')) -> Tagged t a -> f (g (Tagged t b))
   withTaggedF f = fmap coerce . f . coerce
 
   tagInner :: Tagged t (g a) -> g (Tagged t a)
-  default tagInner :: (Coercible (Tagged t (g a)) (g (Tagged t a)))
+  default tagInner :: ( Coercible (Tagged t (g a)) (g (Tagged t a))
+                      , Coercible (g (Tagged t a)) (g a))
                       => Tagged t (g a) -> g (Tagged t a)
   tagInner = coerce
 
