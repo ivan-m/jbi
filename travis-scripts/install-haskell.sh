9001572165e74cdfc22b8ff1050142c88bdf6913
Ivan Lazar Miljenovic <ivan.miljenovic@gmail.com>
2017-09-10 23:58:16 +1000

    travis_retry not available by default in sub-shells

--- travis-scripts/install-haskell.sh
+++ travis-scripts/install-haskell.sh
@@ -13,6 +13,28 @@ then
        > $HOME/.cabal/packages/hackage.haskell.org/00-index.tar;
 fi
 
+# Need to make travis_retry available https://gist.github.com/letmaik/caa0f6cc4375cbfcc1ff26bd4530c2a3
+travis_retry() {
+  local result=0
+  local count=1
+  while [ $count -le 3 ]; do
+    [ $result -ne 0 ] && {
+      echo -e "\n${ANSI_RED}The command \"$@\" failed. Retrying, $count of 3.${ANSI_RESET}\n" >&2
+    }
+    # ! { } ignores set -e, see https://stackoverflow.com/a/4073372
+    ! { "$@"; result=$?; }
+    [ $result -eq 0 ] && break
+    count=$(($count + 1))
+    sleep 1
+  done
+
+  [ $count -gt 3 ] && {
+    echo -e "\n${ANSI_RED}The command \"$@\" failed 3 times.${ANSI_RESET}\n" >&2
+  }
+
+  return $result
+}
+
 travis_retry cabal update -v
 
 sed -i 's/^jobs:/-- jobs:/' ${HOME}/.cabal/config
